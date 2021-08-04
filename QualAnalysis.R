d70e40e775b9625725cdb8298a4139dc4dfe8a92
kharrison89 <55414964+kharrison89@users.noreply.github.com>
2021-08-04 16:19:08 -0600

    updating qual analysis

--- QualAnalysis.R
+++ QualAnalysis.R
@@ -5,12 +5,13 @@ library(RColorBrewer)
 library(wordcloud)
 library(readxl)
 library(ggplot2)
+library(tidyverse)
 
 rm(list=ls())
 
 qualDat <- read_xlsx(file.choose())
 
-qualDat$Shoe <- factor(qualDat$Shoe, c('Tri', 'Asym'))
+qualDat$Shoe <- factor(qualDat$Shoe, c('Lace', 'A','B', 'C'))
 
 qualDat %>%
   pivot_longer(cols = Overall:Heel,
@@ -21,37 +22,83 @@ qualDat %>%
     medAvg = median(Rating)
   )
 
+
+### Probability of higher overall score (for radar plot)
+
+qualDat <- qualDat %>% 
+  group_by(Subject) %>%
+  mutate(z_score = scale(Overall)) %>% # Change to the variable you want to test
+  group_by(Shoe)
+
+runmod <- brm(data = qualDat,
+              family = gaussian,
+              z_score ~ Shoe + (1 | Subject), #fixed effect of configuration and time period with a different intercept and slope for each subject
+              prior = c(prior(normal(0, 1), class = Intercept), #The intercept prior is set as a mean of 25 with an SD of 5 This may be interpreted as the average loading rate (but average is again modified by the subject-specific betas)
+                        prior(normal(0, 1), class = b), #beta for the intercept for the change in loading rate for each configuration
+                        prior(cauchy(0, 1), class = sd), #This is a regularizing prior, meaning we will allow the SD of the betas to vary across subjects
+                        prior(cauchy(0, 1), class = sigma)), #overall variability that is left unexplained 
+              iter = 2000, warmup = 1000, chains = 4, cores = 4,
+              control = list(adapt_delta = .975, max_treedepth = 20),
+              seed = 190831)
+
+posterior <- posterior_samples(runmod)
+
+sum(posterior[,4] < 0) / length(posterior[,4]) 
+
+
+####
+qualDat <- pivot_longer(qualDat, cols = Overall:Heel, names_to = "Location", values_to = "Rating")
+qualDat$Location <- factor(qualDat$Location, c("Overall", "Forefoot", "Midfoot", "Heel"))
+
 qualDat %>%
-  pivot_longer(cols = Overall:Heel,
-               names_to = "Location", values_to = "Rating") %>%
   filter(Location == 'Overall') %>%
-  ggplot(mapping = aes(x = Shoe, y = Rating, group = SubjectName)) + geom_line(aes(color = SubjectName)) + geom_point(aes(color = SubjectName)) 
+  ggplot(mapping = aes(x = Shoe, y = Rating, group = Subject)) + geom_line(aes(color = Subject)) + geom_point(aes(color = Subject)) 
   
 
 
 qualDat %>%
-  pivot_longer(cols = Forefoot:Heel,
-               names_to = "Location", values_to = "Rating") %>%
-  filter(Location != 'Performance') %>%
-  ggplot(mapping = aes(x = Rating, fill = Shoe)) + geom_density(alpha = 0.5) + facet_wrap(~Location) + scale_fill_manual(values=c("#003D4C", "#00966C", "#ECE81A","#DC582A","#CAF0E4"))
+  filter(Location != 'Overall') %>%
+    ggplot(mapping = aes(x = Rating, fill = Shoe)) + geom_density(alpha = 0.5) + facet_wrap(~Location) + scale_fill_manual(values=c("#003D4C", "#00966C", "#ECE81A","#DC582A","#CAF0E4"))
 
+
+
+
+
+  group_by(Subject) %>% 
+  mutate(z_score = scale(Distance)) %>%
+  group_by(Shoe)
+
+datDist <- subset(datDist, datDist$z_score < 2)
+datDist <- subset(datDist, datDist$z_score > -2)
+
+ggplot(data = datDist) + geom_boxplot(mapping = aes(x = Subject, y = Distance, fill = Shoe)) + scale_fill_manual(values=c("#003D4C", "#00966C", "#ECE81A","#DC582A","#CAF0E4")) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+runmod <- brm(data = datDist,
+              family = gaussian,
+              z_score ~ Shoe + (1 + Shoe | Subject), #fixed effect of configuration and time period with a different intercept and slope for each subject
+              prior = c(prior(normal(0, 1), class = Intercept), #The intercept prior is set as a mean of 25 with an SD of 5 This may be interpreted as the average loading rate (but average is again modified by the subject-specific betas)
+                        prior(normal(0, 1), class = b), #beta for the intercept for the change in loading rate for each configuration
+                        prior(cauchy(0, 1), class = sd), #This is a regularizing prior, meaning we will allow the SD of the betas to vary across subjects
+                        prior(cauchy(0, 1), class = sigma)), #overall variability that is left unexplained 
+              iter = 2000, warmup = 1000, chains = 4, cores = 4,
+              control = list(adapt_delta = .975, max_treedepth = 20),
+              seed = 190831)
 
 # making word clouds ------------------------------------------------------
 
-#tri <- subset(qualDat, qualDat$Shoe == 'Tri')
-BOA <- subset(qualDat, qualDat$Shoe == 'BOA')
-lace <- subset(qualDat, qualDat$Shoe == 'Lace')
-#triNotes <- tri$Comments
-BOAnotes <- BOA$Comments
-laceNotes <- lace$Comments
+A <- subset(qualDat, qualDat$Shoe == 'A', GoodComments:BadComments)
+B <- subset(qualDat, qualDat$Shoe == 'B', GoodComments:BadComments)
+C <- subset(qualDat, qualDat$Shoe == 'C', GoodComments:BadComments)
+Lace <- subset(qualDat, qualDat$Shoe == 'Lace', GoodComments:BadComments)
 
+replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]", " ", x))})
 
 makeWordCloud <- function(inputText) {
   
   docs <- Corpus(VectorSource(inputText))
   docs <- docs %>%
     tm_map(removeNumbers) %>%
-    tm_map(removePunctuation)
+    tm_map(replacePunctuation)
   
   # Convert the text to lower case
   docs <- tm_map(docs, content_transformer(tolower))
@@ -62,22 +109,45 @@ makeWordCloud <- function(inputText) {
   # Remove your own stop word
   # specify your stopwords as a character vector
   docs <- tm_map(docs, removeWords, c("like", "feel","feels","lace","bottom","steel","replacement","toe.","toe",
-                                      "felt","tri")) 
+                                      "felt","tri", "na")) 
   
   dtm <- TermDocumentMatrix(docs)
   m <- as.matrix(dtm)
   v <- sort(rowSums(m),decreasing=TRUE)
-  d <- data.frame(word = names(v),freq=v)
-  head(d, 10)
+  
+  vGood <- m[,1]
+  vBad <- m[,2]
+  vBad <- sort(vBad, decreasing = TRUE)
+  vGood <- sort(vGood, decreasing = TRUE)
+  vGood <- as.data.frame(vGood)
+  vBad <- as.data.frame(vBad)
+  
+  colorList <- c(rep('green', nrow(vGood)), rep('red', nrow(vBad)))
+  
+  GoodWords <- rownames(vGood)
+  GoodFrq<- vGood[,1]
+  Good <- cbind(GoodWords, GoodFrq)
+  BadWords <- rownames(vBad)
+  BadFrq <- vBad[,1]
+  Bad <- cbind(BadWords, BadFrq)
+  
+  d <- rbind(Good, Bad)
+  d <- cbind(d, colorList)
+  d <- as.data.frame(d)
+  colnames(d) <- c('Word', 'Freq', 'Color')
+  d <- d[order(d$Freq, decreasing = TRUE),]
+  d <- d[d$Freq > 0, ]
+ 
+  d$Freq <- as.numeric(d$Freq)
   
   set.seed(1234)
-  wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-            max.words=25, random.order=FALSE, rot.per=0.35, 
-            colors=brewer.pal(8, "Dark2"))
+  wordcloud(d$Word, d$Freq, min.freq = 1, max.words = nrow(d),
+            random.order=FALSE, rot.per=0.35, 
+            colors=d$Color, ordered.colors = TRUE)
   
 }
 
 #makeWordCloud(triNotes)
-makeWordCloud(BOAnotes)
-makeWordCloud(laceNotes)
+makeWordCloud(Lace)
+
 
--- RaderPlots.R
+++ RaderPlots.R
@@ -1,12 +1,12 @@
 library(fmsb)
 
-agility <- 72
+agility <- 22
   
-power <- 68
+power <- 52
   
-fit <- 85
+fit <- 50
   
-endurance <- 57
+endurance <- 41
 
 data <- t(c(agility, power, fit, endurance))
 
@@ -14,14 +14,16 @@ data <- t(c(agility, power, fit, endurance))
 
 data <- as.data.frame(data)
 
-thresh<- as.data.frame(t(rep(60, 4)))
+improvThresh<- as.data.frame(t(rep(70, 4)))
+equalThresh<- as.data.frame(t(rep(50,4)))
+
 min =as.data.frame(t(rep(0, 4)))
 max = as.data.frame(t(rep(100,4)))
-data <- rbind(max, min, thresh, data)
+data <- rbind(max, min, improvThresh, equalThresh, data)
 
 colnames(data) <- c("Agility: Contact Time", "Power: Propulsive Force", "Fit: Overall Qualitative Score", "Endurance: Less wasted force")
 
-colors <- c("#53565A", "#DC582A")
+colors <- c("#C8C9C7","#53565A", "#DC582A")
 
 create_beautiful_radarchart <- function(data, color = "#00AFBB", 
                                         vlabels = colnames(data), vlcex = 0.7,
@@ -43,7 +45,57 @@ create_beautiful_radarchart <- function(data, color = "#00AFBB",
 
 create_beautiful_radarchart(data = data, color = colors)
 
-legend(x = "bottomleft", legend = c("threshold for improvement", "test shoe scores relative to baseline"),
-       bty = "n", pch = 20, col = colors, text.col = "black", cex = 1, pt.cex = 1.5)
+legend(x = "topright", inset = c(- 0.1, 0), legend = c("threshold for confidence in improvement", "Tri Performance", "Asym Performance"),
+       bty = "n", pch = 20, col = colors, text.col = "black", cex = .7, pt.cex = 1)
 
+
+
+############################## For Golf ##########################################
+
+power <- 23
+
+precision <- 84
+
+fit <- 49
+
+
+data <- t(c(power, precision, fit))
+
+
+
+data <- as.data.frame(data)
+
+improvThresh<- as.data.frame(t(rep(70, 3)))
+equalThresh<- as.data.frame(t(rep(50,3)))
+
+min =as.data.frame(t(rep(0, 3)))
+max = as.data.frame(t(rep(100,3)))
+data <- rbind(max, min, improvThresh, equalThresh, data)
+
+colnames(data) <- c("Power: Drive Distance", "Precision: Drive consistency and Accuracy", "Fit: Overall Qualitative Score")
+
+colors <- c("#C8C9C7","#53565A", "#DC582A")
+
+create_beautiful_radarchart <- function(data, color = "#00AFBB", 
+                                        vlabels = colnames(data), vlcex = 0.7,
+                                        caxislabels = NULL, title = NULL, ...){
+  radarchart(
+    data, axistype = 1,
+    # Customize the polygon
+    pcol = color, pfcol = scales::alpha(color, 0.5), plwd = 2, plty = 1,
+    # Customize the grid
+    cglcol = "grey", cglty = 1, cglwd = 0.8,
+    # Customize the axis
+    axislabcol = "grey", 
+    # Variable labels
+    vlcex = vlcex, vlabels = vlabels,
+    caxislabels = caxislabels, title = title, ...
+  )
+}
+
+
+create_beautiful_radarchart(data = data, color = colors)
+
+legend(x = "topright", inset = c(- 0.1, 0), legend = c("threshold for confidence in improvement", "Lace Performance", "Heel Config A Performance"),
+       bty = "n", pch = 20, col = colors, text.col = "black", cex = .7, pt.cex = 1)
 
