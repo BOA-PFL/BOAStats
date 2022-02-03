7b95ab1f6765fd6f139f50850f4b77c529d22623
kharrison89 <55414964+kharrison89@users.noreply.github.com>
2022-02-03 13:45:56 -0700

    another tweak for Bayesian analyses

--- PerformanceTest_CyclingBayesianAnalysis.R
+++ PerformanceTest_CyclingBayesianAnalysis.R
@@ -43,15 +43,27 @@ withinSubPlot <- function(inputDF, colName, dir) {
   
 }
 
-
 extractVals <- function(dat, mod, configNames, var, dir) {
   
-  for (configName in configNames) {
+  #configNames = otherConfigs
+  #mod = runmod
+  #dir = 'higher'
+  #var = 'CarryFlatLength'
+  
+  Config = rep(NA, length(configNames))
+  ProbImp = matrix(0, length(configNames))
+  lowCI = matrix(0, length(configNames))
+  highCI = matrix(0, length(configNames))
+  
+  for (i in 1:length(configNames)) {
     # This function takes the original dataframe (dat, same one entered into runmod), the Bayesian model from brms (runmod), 
     # the configuration Name, and the variable you are testing. It returns:
     # [1] the probabality the variable was better in the test config vs. the baseline config
     # [3] the lower bound of the bayesian 95% posterior interval (as percent change from baseline) 
     # [4] the upper bound of the bayesian 95% posterior interval (as percent change from baseline)
+    #i = 1
+    
+    configName = configNames[i]
     configColName <- paste('b_Config', configName, sep = "")
     posterior <- posterior_samples(mod)
     
@@ -76,10 +88,18 @@ extractVals <- function(dat, mod, configNames, var, dir) {
     ci_LowPct <- meanSD*ciLow/mean*100
     ci_HighPct <- meanSD*ciHigh/mean*100
     
-    return(list('Config:', configName, 'Probability of Improvement:', prob, 'Worse end of CI:', ci_LowPct, 'Best end of CI:', ci_HighPct))
-    
+    output = list('Config:', configName, 'Probability of Improvement:', prob, 'Worse end of CI:', ci_LowPct, 'Best end of CI:', ci_HighPct)
+    Config[i] = configName
+    ProbImp[i] = prob
+    lowCI[i] = ci_LowPct
+    highCI[i] = ci_HighPct
   }
-  
+  ProbImp = round(ProbImp, 2)
+  lowCI = round(lowCI, 1)
+  highCI = round(highCI,1)
+  output = cbind(Config, ProbImp, lowCI, highCI)
+  colnames(output) = c('Config', 'Probability of Improvement', 'Low end of CI', 'High end of CI')
+  return(output)
 }
 
 
@@ -89,9 +109,9 @@ dat <- read_csv(file.choose())
 
 dat <- as_tibble(dat)
 
-baseline <- 'Velcro'
+baseline <- 'DD'
 
-otherConfigs <- c('DD')
+otherConfigs <- c('HL', 'PFW')
 
 allConfigs <- c(baseline, otherConfigs)
 
@@ -154,6 +174,64 @@ runmod <- brm(data = dat,
 
 
 extractVals(dat, runmod, otherConfigs, 'sprintPower', 'higher') 
+
+
+######################### Pressure
+
+### Heel pressure variation across time throughout pedal stroke
+dat <- dat %>% 
+  group_by(Subject) %>%
+  mutate(z_score = scale(overallHeelVar)) %>% 
+  group_by(Config)
+
+ggplot(data = dat, aes(x = overallHeelVar)) + geom_histogram() + facet_wrap(~Subject) #Change x axis variable to your variable of interest. Check for normal-ish distribution.
+
+
+withinSubPlot(dat, colName = 'overallHeelVar', dir = 'lower') 
+
+
+runmod <- brm(data = dat, 
+              family = gaussian,
+              z_score ~ Config + (1 + Config| Subject), #fixed effect of configuration and time period with a different intercept and slope for each subject
+              prior = c(prior(normal(0, 1), class = Intercept), #The intercept prior is set as a mean of 25 with an SD of 5 This may be interpreted as the average loading rate (but average is again modified by the subject-specific betas)
+                        prior(normal(0, 1), class = b), #beta for the intercept for the change in loading rate for each configuration
+                        prior(cauchy(0, 1), class = sd), #This is a regularizing prior, meaning we will allow the SD of the betas to vary across subjects
+                        prior(cauchy(0, 1), class = sigma)), #overall variability that is left unexplained 
+              iter = 2000, warmup = 1000, chains = 4, cores = 4,
+              control = list(adapt_delta = .975, max_treedepth = 20),
+              seed = 190831)
+
+
+extractVals(dat, runmod, otherConfigs, 'overallHeelVar', 'lower')
+
+### Peak pressure throughout pedal stroke 
+
+dat <- dat %>% 
+  group_by(Subject) %>%
+  mutate(z_score = scale(overallPeakP)) %>% 
+  group_by(Config)
+
+ggplot(data = dat, aes(x = overallPeakP)) + geom_histogram() + facet_wrap(~Subject) #Change x axis variable to your variable of interest. Check for normal-ish distribution.
+
+
+withinSubPlot(dat, colName = 'overallPeakP', dir = 'lower') 
+
+
+runmod <- brm(data = dat, 
+              family = gaussian,
+              z_score ~ Config + (1 + Config| Subject), #fixed effect of configuration and time period with a different intercept and slope for each subject
+              prior = c(prior(normal(0, 1), class = Intercept), #The intercept prior is set as a mean of 25 with an SD of 5 This may be interpreted as the average loading rate (but average is again modified by the subject-specific betas)
+                        prior(normal(0, 1), class = b), #beta for the intercept for the change in loading rate for each configuration
+                        prior(cauchy(0, 1), class = sd), #This is a regularizing prior, meaning we will allow the SD of the betas to vary across subjects
+                        prior(cauchy(0, 1), class = sigma)), #overall variability that is left unexplained 
+              iter = 2000, warmup = 1000, chains = 4, cores = 4,
+              control = list(adapt_delta = .975, max_treedepth = 20),
+              seed = 190831)
+
+
+extractVals(dat, runmod, otherConfigs, 'overallPeakP', 'lower')
+
+
 
 
 
