4de78ac84a1021b56942d06d10935c0c4b2bf9a4
Kate Harrison <55414964+kharrison89@users.noreply.github.com>
2023-04-18 15:55:48 -0600

    Create BigData_updateAgilityPressure.R

---
+++ BigData_updateAgilityPressure.R
@@ -0,0 +1,38 @@
+### Load libraries
+
+library(tidyverse)
+library(brms)
+library(tidybayes)
+library(lme4)
+library(dplyr)
+library(rlang)
+library(reshape2)
+
+rm(list=ls())
+
+
+### Import Static Pressure data
+
+ChildDat <- read.csv(file.choose())
+
+ChildDat <- subset(ChildDat, ChildDat$Movement == ('cmj')|ChildDat$Movement == ('skater'))
+
+#staticDat$Subject[stati
+ChildDat$Year <- rep(2023, dim(ChildDat)[1])
+ChildDat$Month <- rep('January', dim(ChildDat)[1])
+ChildDat$Brand <- rep('NoBull', dim(ChildDat)[1])
+ChildDat$Model <- rep('Trainer', dim(ChildDat)[1]) 
+#childDat$Subject == 'WesWebber'] <- 'WesWeber'
+
+
+
+a <- winDialog(type = 'yesno', message = 'Have you checked the Child Dataframe?')
+if (a == 'YES'){
+  # Check the Child Data before!!
+  write.table(ChildDat, file = 'C:/Users/kate.harrison/Boa Technology Inc/PFL Team - General/BigData/DB_V2/AgilityPressureDB.csv', sep = ',',
+              append = TRUE,col.names = FALSE, row.names = FALSE)
+}
+
+
+
+
