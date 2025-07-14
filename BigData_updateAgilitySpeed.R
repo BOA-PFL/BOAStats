211bc7ecde4674a8a9da680606e6d94d8eea5db0
milenasingletary <105753587+milenasingletary@users.noreply.github.com>
2025-07-14 15:41:54 -0600

    Update BigData_updateAgilitySpeed.R

--- BigData_updateAgilitySpeed.R
+++ BigData_updateAgilitySpeed.R
@@ -297,13 +297,13 @@ rm(ParentDat,ChildDat,name_order,a)
 ## Agility Pressure 
 
 ParentDat <- read.csv('\\\\boa-srv10\\PFL-DATA\\BigData\\DB_V2\\AgilityPressureDB.csv',nrows=1)
-#ParentDat <- read.csv('Z:/BigData/DB_V2/AgilityPressureDB.csv',nrows=1)
+ParentDat <- read.csv('Z:/BigData/DB_V2/AgilityPressureDB.csv',nrows=1)
 
 name_order = colnames(ParentDat)
 
 ### Import Dynamic Pressure data
 
-ChildDat <- read.csv('\\\\boa-srv10\\PFL-DATA\\Testing Segments\\AgilityPerformanceData\\AS_Train_UpperXPanelStiffnessI_Mech_Sept24\\XSENSOR\\cropped\\0_CompiledResults.csv')
+ChildDat <- read.csv('Z:\\Testing Segments\\AgilityPerformanceData\\2025_Mechanistic_WomensPanelStiffnessII_Adidas\\Xsensor\\cropped\\1_CompiledResults.csv')
 
 ChildDat <- subset(ChildDat, ChildDat$Movement == ('cmj')|ChildDat$Movement == ('skater'))
 
@@ -312,11 +312,41 @@ ChildDat <- ChildDat %>%
   summarise_all(mean)
 
 #staticDat$Subject[stati
-ChildDat$Year <- rep(2023, dim(ChildDat)[1])
-ChildDat$Month <- rep('January', dim(ChildDat)[1])
-ChildDat$Brand <- rep('NoBull', dim(ChildDat)[1])
-ChildDat$Model <- rep('Trainer', dim(ChildDat)[1]) 
-#childDat$Subject == 'WesWebber'] <- 'WesWeber'
+noSub <- length(ChildDat$Subject)
+ChildDat$Year <- rep(2025, dim(ChildDat)[1])
+ChildDat$Month <- rep('May', dim(ChildDat)[1])
+ChildDat$Brand <- rep('Adidas', dim(ChildDat)[1])
+ChildDat$Model <- rep('Barricade', dim(ChildDat)[1]) 
+ChildDat$toeArea_mid <- rep('NA', each = noSub)
+ChildDat$ffP_late <- rep('NA', each = noSub)
+ChildDat$ffArea_late <- rep('NA', each = noSub)
+ChildDat$ffP_Mid <- rep('NA', each = noSub)
+ChildDat$ffArea_Mid <- rep('NA', each = noSub)
+ChildDat$ffPMax_late <- rep('NA', each = noSub)
+
+ChildDat$mfP_late <- rep('NA', each = noSub)
+ChildDat$mfArea_late <- rep('NA', each = noSub)
+ChildDat$mfP_Mid <- rep('NA', each = noSub)
+ChildDat$mfArea_Mid <- rep('NA', each = noSub)
+ChildDat$latP_late <- rep('NA', each = noSub)
+ChildDat$latArea_late <- rep('NA', each = noSub)
+
+
+ChildDat$medP_late <- rep('NA', each = noSub)
+ChildDat$medArea_late <- rep('NA', each = noSub)
+
+ChildDat$ffDorsalEarlyP <- rep('NA', each = noSub)
+ChildDat$ffDorsalMidP <- rep('NA', each = noSub)
+ChildDat$ffDorsalLateP <- rep('NA', each = noSub)
+ChildDat$mfDorsalEarlyP <- rep('NA', each = noSub)
+ChildDat$mfDorsalMidP <- rep('NA', each = noSub)
+ChildDat$mfDorsalLateP <- rep('NA', each = noSub)
+ChildDat$instepEarlyP <- rep('NA', each = noSub)
+ChildDat$instepMidP <- rep('NA', each = noSub)
+ChildDat$instepLateP <- rep('NA', each = noSub)
+ChildDat$ffDorsalMax <- rep('NA', each = noSub)
+ChildDat$mfDorsalMax <- rep('NA', each = noSub)
+ChildDat$instepMax <- rep('NA', each = noSub)
 
 ChildDat <- ChildDat[,name_order]
 
