36617a4cd0019c0d47958c39aaa2305824ba963d
milenasingletary <105753587+milenasingletary@users.noreply.github.com>
2026-06-11 17:00:35 -0600

    Update BigData_updateAgilityPressure.R

--- BigData_updateAgilityPressure.R
+++ BigData_updateAgilityPressure.R
@@ -10,28 +10,60 @@ library(reshape2)
 
 rm(list=ls())
 
-### Import Parent Data
+## Update Agility Pressure 
 
-ParentDat <- read.csv('C:/Users/kate.harrison/Boa Technology Inc/PFL Team - General/BigData/DB_V2/AgilityPressureDB.csv',nrows=1)
+
+### Import Parent Data
+ParentDat <- read.csv('Z:/BigData/DB_V2/AgilityPressureDB.csv',nrows=1)
 
 name_order = colnames(ParentDat)
 
 ### Import Dynamic Pressure data
 
-ChildDat <- read.csv(file.choose())
+ChildDat <- read.csv('Z:\\Testing Segments\\AgilityPerformanceData\\2025_Mechanistic_WomensPanelStiffnessII_Adidas\\Xsensor\\cropped\\1_CompiledResults.csv')
 
 ChildDat <- subset(ChildDat, ChildDat$Movement == ('cmj')|ChildDat$Movement == ('skater'))
 
 ChildDat <- ChildDat %>%
-  group_by(Subject, Config, Movement) %>%
+  group_by(Subject, Config, Order, Movement) %>%
   summarise_all(mean)
 
 #staticDat$Subject[stati
-ChildDat$Year <- rep(2023, dim(ChildDat)[1])
-ChildDat$Month <- rep('January', dim(ChildDat)[1])
-ChildDat$Brand <- rep('NoBull', dim(ChildDat)[1])
-ChildDat$Model <- rep('Trainer', dim(ChildDat)[1]) 
-#childDat$Subject == 'WesWebber'] <- 'WesWeber'
+noSub <- length(ChildDat$Subject)
+ChildDat$Year <- rep(2025, dim(ChildDat)[1])
+ChildDat$Month <- rep('May', dim(ChildDat)[1])
+ChildDat$Brand <- rep('Adidas', dim(ChildDat)[1])
+ChildDat$Model <- rep('Barricade', dim(ChildDat)[1]) 
+ChildDat$toeArea_mid <- rep('NA', each = noSub)
+ChildDat$ffP_late <- rep('NA', each = noSub)
+ChildDat$ffArea_late <- rep('NA', each = noSub)
+ChildDat$ffP_Mid <- rep('NA', each = noSub)
+ChildDat$ffArea_Mid <- rep('NA', each = noSub)
+ChildDat$ffPMax_late <- rep('NA', each = noSub)
+
+ChildDat$mfP_late <- rep('NA', each = noSub)
+ChildDat$mfArea_late <- rep('NA', each = noSub)
+ChildDat$mfP_Mid <- rep('NA', each = noSub)
+ChildDat$mfArea_Mid <- rep('NA', each = noSub)
+ChildDat$latP_late <- rep('NA', each = noSub)
+ChildDat$latArea_late <- rep('NA', each = noSub)
+
+
+ChildDat$medP_late <- rep('NA', each = noSub)
+ChildDat$medArea_late <- rep('NA', each = noSub)
+
+ChildDat$ffDorsalEarlyP <- rep('NA', each = noSub)
+ChildDat$ffDorsalMidP <- rep('NA', each = noSub)
+ChildDat$ffDorsalLateP <- rep('NA', each = noSub)
+ChildDat$mfDorsalEarlyP <- rep('NA', each = noSub)
+ChildDat$mfDorsalMidP <- rep('NA', each = noSub)
+ChildDat$mfDorsalLateP <- rep('NA', each = noSub)
+ChildDat$instepEarlyP <- rep('NA', each = noSub)
+ChildDat$instepMidP <- rep('NA', each = noSub)
+ChildDat$instepLateP <- rep('NA', each = noSub)
+ChildDat$ffDorsalMax <- rep('NA', each = noSub)
+ChildDat$mfDorsalMax <- rep('NA', each = noSub)
+ChildDat$instepMax <- rep('NA', each = noSub)
 
 ChildDat <- ChildDat[,name_order]
 
@@ -39,10 +71,10 @@ ChildDat <- ChildDat[,name_order]
 a <- winDialog(type = 'yesno', message = 'Have you checked the Child Dataframe?')
 if (a == 'YES'){
   # Check the Child Data before!!
-  write.table(ChildDat, file = 'C:/Users/kate.harrison/Boa Technology Inc/PFL Team - General/BigData/DB_V2/AgilityPressureDB.csv', sep = ',',
+  write.table(ChildDat, file = 'Z:/BigData/DB_V2/AgilityPressureDB.csv', sep = ',',
               append = TRUE,col.names = FALSE, row.names = FALSE)
 }
 
-
+rm(ParentDat,ChildDat,name_order,a)
 
 
